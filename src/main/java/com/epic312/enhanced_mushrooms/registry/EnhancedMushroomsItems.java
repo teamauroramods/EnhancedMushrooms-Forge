2300f2c8296d0357f3ae6ec006bd3255353eff18
Epic312 <kaden_ostendorf@yahoo.com>
2020-05-13 15:59:48 -0600

    Boats are in!

--- src/main/java/com/epic312/enhanced_mushrooms/registry/EnhancedMushroomsItems.java
+++ src/main/java/com/epic312/enhanced_mushrooms/registry/EnhancedMushroomsItems.java
@@ -1,11 +1,17 @@
 package com.epic312.enhanced_mushrooms.registry;
 
 import com.epic312.enhanced_mushrooms.EnhancedMushrooms;
+import com.teamabnormals.abnormals_core.core.utils.RegistryHelper;
 import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
-// Registry code originally written by bageldotjpg for the Atmospheric mod
+@Mod.EventBusSubscriber(modid = EnhancedMushrooms.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class EnhancedMushroomsItems {
-    public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, EnhancedMushrooms.MODID);
+    public static final RegistryHelper HELPER = EnhancedMushrooms.REGISTRY_HELPER;
+
+    public static final RegistryObject<Item> RED_MUSHROOM_BOAT = HELPER.createBoatItem("red_mushroom", EnhancedMushroomsBlocks.RED_MUSHROOM_PLANKS);
+    public static final RegistryObject<Item> BROWN_MUSHROOM_BOAT = HELPER.createBoatItem("brown_mushroom", EnhancedMushroomsBlocks.BROWN_MUSHROOM_PLANKS);
 }
