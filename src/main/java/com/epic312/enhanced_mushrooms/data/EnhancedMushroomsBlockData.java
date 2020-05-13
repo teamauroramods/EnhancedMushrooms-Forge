0a7729b52b0bf15a02bc82e58de1470adbe914cb
Epic312 <kaden_ostendorf@yahoo.com>
2020-05-13 14:30:43 -0600

    The world shall burn

--- src/main/java/com/epic312/enhanced_mushrooms/data/EnhancedMushroomsBlockData.java
+++ src/main/java/com/epic312/enhanced_mushrooms/data/EnhancedMushroomsBlockData.java
@@ -1,5 +1,6 @@
 package com.epic312.enhanced_mushrooms.data;
 
+import com.epic312.enhanced_mushrooms.EnhancedMushrooms;
 import com.epic312.enhanced_mushrooms.registry.EnhancedMushroomsBlocks;
 import com.teamabnormals.abnormals_core.core.utils.DataUtils;
 import net.minecraft.client.renderer.RenderType;
@@ -7,14 +8,26 @@ import net.minecraft.client.renderer.RenderTypeLookup;
 
 public class EnhancedMushroomsBlockData {
     public static void registerFlammables() {
+        // Red Mushrooms
         DataUtils.registerFlammable(EnhancedMushroomsBlocks.RED_MUSHROOM_STEM.get(), 5, 5);
         DataUtils.registerFlammable(EnhancedMushroomsBlocks.RED_MUSHROOM_HYPHAE.get(), 5, 5);
         DataUtils.registerFlammable(EnhancedMushroomsBlocks.STRIPPED_RED_MUSHROOM_STEM.get(), 5, 5);
         DataUtils.registerFlammable(EnhancedMushroomsBlocks.STRIPPED_RED_MUSHROOM_HYPHAE.get(), 5, 5);
+        DataUtils.registerFlammable(EnhancedMushroomsBlocks.RED_MUSHROOM_PLANKS.get(), 5, 20);
+        DataUtils.registerFlammable(EnhancedMushroomsBlocks.RED_MUSHROOM_SLAB.get(), 5, 20);
+        DataUtils.registerFlammable(EnhancedMushroomsBlocks.RED_MUSHROOM_STAIRS.get(), 5, 20);
+        DataUtils.registerFlammable(EnhancedMushroomsBlocks.RED_MUSHROOM_FENCE.get(), 5, 20);
+        DataUtils.registerFlammable(EnhancedMushroomsBlocks.RED_MUSHROOM_FENCE_GATE.get(), 5, 20);
+        // Brown Mushrooms
         DataUtils.registerFlammable(EnhancedMushroomsBlocks.BROWN_MUSHROOM_STEM.get(), 5, 5);
         DataUtils.registerFlammable(EnhancedMushroomsBlocks.BROWN_MUSHROOM_HYPHAE.get(), 5, 5);
         DataUtils.registerFlammable(EnhancedMushroomsBlocks.STRIPPED_BROWN_MUSHROOM_STEM.get(), 5, 5);
         DataUtils.registerFlammable(EnhancedMushroomsBlocks.STRIPPED_BROWN_MUSHROOM_HYPHAE.get(), 5, 5);
+        DataUtils.registerFlammable(EnhancedMushroomsBlocks.BROWN_MUSHROOM_PLANKS.get(), 5, 20);
+        DataUtils.registerFlammable(EnhancedMushroomsBlocks.BROWN_MUSHROOM_SLAB.get(), 5, 20);
+        DataUtils.registerFlammable(EnhancedMushroomsBlocks.BROWN_MUSHROOM_STAIRS.get(), 5, 20);
+        DataUtils.registerFlammable(EnhancedMushroomsBlocks.BROWN_MUSHROOM_FENCE.get(), 5, 20);
+        DataUtils.registerFlammable(EnhancedMushroomsBlocks.BROWN_MUSHROOM_FENCE_GATE.get(), 5, 20);
     }
 
     public static void setupRenderLayer() {
