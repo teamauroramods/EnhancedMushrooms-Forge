d00a093a3c7421609e34cee654d04c56ab097cf2
Epic312 <kaden_ostendorf@yahoo.com>
2020-05-14 17:36:57 -0600

    Natural giant mushroom generation Also uncredited Bagel since pretty much none of his original code is still in here lmao

--- src/main/java/com/epic312/enhanced_mushrooms/registry/EnhancedMushroomsFeatures.java
+++ src/main/java/com/epic312/enhanced_mushrooms/registry/EnhancedMushroomsFeatures.java
@@ -1,8 +1,13 @@
 package com.epic312.enhanced_mushrooms.registry;
 
 import com.epic312.enhanced_mushrooms.world.biome.EnMushroomsBiomeFeatures;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.Biomes;
+import net.minecraft.world.biome.DefaultBiomeFeatures;
+import net.minecraft.world.gen.blockstateprovider.SimpleBlockStateProvider;
+import net.minecraft.world.gen.feature.BigMushroomFeatureConfig;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.registries.ForgeRegistries;
 
