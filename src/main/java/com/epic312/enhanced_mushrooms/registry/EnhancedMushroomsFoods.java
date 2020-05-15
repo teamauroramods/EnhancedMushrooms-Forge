a57eda372996f5cd4dadd7be7bae8eb99370c4c5
Epic312 <kaden_ostendorf@yahoo.com>
2020-05-14 22:22:01 -0600

    Added glowshroom textures

---
+++ src/main/java/com/epic312/enhanced_mushrooms/registry/EnhancedMushroomsFoods.java
@@ -0,0 +1,10 @@
+package com.epic312.enhanced_mushrooms.registry;
+
+import com.epic312.enhanced_mushrooms.EnhancedMushrooms;
+import net.minecraft.item.Food;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = EnhancedMushrooms.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class EnhancedMushroomsFoods {
+    public static Food ROASTED_MUSHROOMS = new Food.Builder().hunger(4).saturation((0.2F)).build();
+}
--- src/main/java/com/epic312/enhanced_mushrooms/registry/EnhancedMushroomsItems.java
+++ src/main/java/com/epic312/enhanced_mushrooms/registry/EnhancedMushroomsItems.java
@@ -3,6 +3,7 @@ package com.epic312.enhanced_mushrooms.registry;
 import com.epic312.enhanced_mushrooms.EnhancedMushrooms;
 import com.teamabnormals.abnormals_core.core.utils.RegistryHelper;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
@@ -14,4 +15,6 @@ public class EnhancedMushroomsItems {
 
     public static final RegistryObject<Item> RED_MUSHROOM_BOAT = HELPER.createBoatItem("red_mushroom", EnhancedMushroomsBlocks.RED_MUSHROOM_PLANKS);
     public static final RegistryObject<Item> BROWN_MUSHROOM_BOAT = HELPER.createBoatItem("brown_mushroom", EnhancedMushroomsBlocks.BROWN_MUSHROOM_PLANKS);
+
+    public static final RegistryObject<Item> ROASTED_MUSHROOMS = HELPER.createItem("roasted_mushrooms", ()->new Item(new Item.Properties().food(EnhancedMushroomsFoods.ROASTED_MUSHROOMS).group(ItemGroup.FOOD)));
 }
