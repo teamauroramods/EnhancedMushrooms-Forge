2f440328910e80fd0dcb1d3d345bd88fe7cdd3bd
Epic312 <kaden_ostendorf@yahoo.com>
2020-05-31 14:59:33 -0600

    Nerfed roasted mushrooms slightly

--- src/main/java/com/epic312/enhanced_mushrooms/core/registry/EnhancedMushroomsFoods.java
+++ src/main/java/com/epic312/enhanced_mushrooms/core/registry/EnhancedMushroomsFoods.java
@@ -6,6 +6,6 @@ import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = EnhancedMushrooms.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class EnhancedMushroomsFoods {
-    public static Food ROASTED_MUSHROOMS = new Food.Builder().hunger(4).saturation((0.2F)).build();
+    public static Food ROASTED_MUSHROOMS = new Food.Builder().hunger(2).saturation((0.2F)).build();
     public static Food BEEF_STEW = new Food.Builder().hunger(10).saturation(1.0F).build();
 }
