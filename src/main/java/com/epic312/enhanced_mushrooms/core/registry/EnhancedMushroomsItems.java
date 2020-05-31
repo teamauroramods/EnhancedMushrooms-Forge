ff155f048df7c8f272346f2739aee170cfa3070e
Epic312 <kaden_ostendorf@yahoo.com>
2020-05-31 15:07:29 -0600

    Beef stew now works like stew

--- src/main/java/com/epic312/enhanced_mushrooms/core/registry/EnhancedMushroomsItems.java
+++ src/main/java/com/epic312/enhanced_mushrooms/core/registry/EnhancedMushroomsItems.java
@@ -4,6 +4,7 @@ import com.epic312.enhanced_mushrooms.core.EnhancedMushrooms;
 import com.epic312.enhanced_mushrooms.core.registry.util.EMRegistryHelper;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.SoupItem;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 
@@ -16,5 +17,5 @@ public class EnhancedMushroomsItems {
     public static final RegistryObject<Item> GLOWSHROOM_BOAT = HELPER.createCompatBoatItem("quark","glowshroom", EnhancedMushroomsBlocks.GLOWSHROOM_PLANKS);
 
     public static final RegistryObject<Item> ROASTED_MUSHROOMS = HELPER.createItem("roasted_mushrooms", ()->new Item(new Item.Properties().food(EnhancedMushroomsFoods.ROASTED_MUSHROOMS).group(ItemGroup.FOOD)));
-    public static final RegistryObject<Item> BEEF_STEW = HELPER.createItem("beef_stew", ()->new Item(new Item.Properties().food(EnhancedMushroomsFoods.BEEF_STEW).group(ItemGroup.FOOD)));
+    public static final RegistryObject<Item> BEEF_STEW = HELPER.createItem("beef_stew", ()->new SoupItem(new Item.Properties().maxStackSize(1).food(EnhancedMushroomsFoods.BEEF_STEW).group(ItemGroup.FOOD)));
 }
