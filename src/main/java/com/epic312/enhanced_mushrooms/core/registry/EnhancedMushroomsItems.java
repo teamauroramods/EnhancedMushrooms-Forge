4d41ab9079d6753be51870cce8d3b131a2046e0c
Epic312 <kaden_ostendorf@yahoo.com>
2020-07-05 16:03:36 -0600

    Crimson & Warped Boats

--- src/main/java/com/epic312/enhanced_mushrooms/core/registry/EnhancedMushroomsItems.java
+++ src/main/java/com/epic312/enhanced_mushrooms/core/registry/EnhancedMushroomsItems.java
@@ -2,6 +2,7 @@ package com.epic312.enhanced_mushrooms.core.registry;
 
 import com.epic312.enhanced_mushrooms.core.EnhancedMushrooms;
 import com.epic312.enhanced_mushrooms.core.registry.util.EMRegistryHelper;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.SoupItem;
@@ -14,6 +15,8 @@ public class EnhancedMushroomsItems {
 
     public static final RegistryObject<Item> RED_MUSHROOM_BOAT = HELPER.createBoatItem("red_mushroom", EnhancedMushroomsBlocks.RED_MUSHROOM_PLANKS);
     public static final RegistryObject<Item> BROWN_MUSHROOM_BOAT = HELPER.createBoatItem("brown_mushroom", EnhancedMushroomsBlocks.BROWN_MUSHROOM_PLANKS);
+    public static final RegistryObject<Item> CRIMSON_MUSHROOM_BOAT = HELPER.createBoatItem("crimson", EnhancedMushroomsBlocks.BROWN_MUSHROOM_PLANKS); // TODO: these need to drop correct planks at some point
+    public static final RegistryObject<Item> WARPED_MUSHROOM_BOAT = HELPER.createBoatItem("warped", EnhancedMushroomsBlocks.BROWN_MUSHROOM_PLANKS);
     public static final RegistryObject<Item> GLOWSHROOM_BOAT = HELPER.createCompatBoatItem("quark","glowshroom", EnhancedMushroomsBlocks.GLOWSHROOM_PLANKS);
 
     public static final RegistryObject<Item> ROASTED_MUSHROOMS = HELPER.createItem("roasted_mushrooms", ()->new Item(new Item.Properties().food(EnhancedMushroomsFoods.ROASTED_MUSHROOMS).group(ItemGroup.FOOD)));
