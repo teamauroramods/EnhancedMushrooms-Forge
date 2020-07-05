90a5dc2862057aa8a345e941eb97e555a43f6048
Epic312 <kaden_ostendorf@yahoo.com>
2020-07-05 17:42:37 -0600

    removed crimson and warped boats since they're gonna be in nether extension

--- src/main/java/com/epic312/enhanced_mushrooms/core/registry/EnhancedMushroomsItems.java
+++ src/main/java/com/epic312/enhanced_mushrooms/core/registry/EnhancedMushroomsItems.java
@@ -15,8 +15,6 @@ public class EnhancedMushroomsItems {
 
     public static final RegistryObject<Item> RED_MUSHROOM_BOAT = HELPER.createBoatItem("red_mushroom", EnhancedMushroomsBlocks.RED_MUSHROOM_PLANKS);
     public static final RegistryObject<Item> BROWN_MUSHROOM_BOAT = HELPER.createBoatItem("brown_mushroom", EnhancedMushroomsBlocks.BROWN_MUSHROOM_PLANKS);
-    public static final RegistryObject<Item> CRIMSON_MUSHROOM_BOAT = HELPER.createBoatItem("crimson", EnhancedMushroomsBlocks.BROWN_MUSHROOM_PLANKS); // TODO: these need to drop correct planks at some point
-    public static final RegistryObject<Item> WARPED_MUSHROOM_BOAT = HELPER.createBoatItem("warped", EnhancedMushroomsBlocks.BROWN_MUSHROOM_PLANKS);
     public static final RegistryObject<Item> GLOWSHROOM_BOAT = HELPER.createCompatBoatItem("quark","glowshroom", EnhancedMushroomsBlocks.GLOWSHROOM_PLANKS);
 
     public static final RegistryObject<Item> ROASTED_MUSHROOMS = HELPER.createItem("roasted_mushrooms", ()->new Item(new Item.Properties().food(EnhancedMushroomsFoods.ROASTED_MUSHROOMS).group(ItemGroup.FOOD)));
