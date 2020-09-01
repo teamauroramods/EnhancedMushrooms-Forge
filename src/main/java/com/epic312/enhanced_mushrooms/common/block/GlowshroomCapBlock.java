851743a62fb938788c1d1c962a8a60fa13660448
Epic312 <kaden_ostendorf@yahoo.com>
2020-09-01 11:21:03 -0600

    glowshroom caps now work properly

--- src/main/java/com/epic312/enhanced_mushrooms/common/block/GlowshroomCapBlock.java
+++ src/main/java/com/epic312/enhanced_mushrooms/common/block/GlowshroomCapBlock.java
@@ -2,29 +2,40 @@ package com.epic312.enhanced_mushrooms.common.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.HugeMushroomBlock;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.StateContainer;
+import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
-public class GlowshroomCapBlock extends AlphaBlock {
-    public static final BooleanProperty WEST = BooleanProperty.create("west");
-    public static final BooleanProperty EAST = BooleanProperty.create("east");
-    public static final BooleanProperty NORTH = BooleanProperty.create("north");
-    public static final BooleanProperty SOUTH = BooleanProperty.create("south");
-    public static final BooleanProperty UP = BooleanProperty.create("up");
-    public static final BooleanProperty DOWN = BooleanProperty.create("down");
+import java.util.Random;
+
+public class GlowshroomCapBlock extends HugeMushroomBlock {
 
     public GlowshroomCapBlock(Properties properties) {
         super(properties);
     }
 
+    @SuppressWarnings("deprecation")
     @Override
-    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
-        super.fillStateContainer(builder);
-        builder.add(WEST);
-        builder.add(EAST);
-        builder.add(NORTH);
-        builder.add(SOUTH);
-        builder.add(UP);
-        builder.add(DOWN);
+    public boolean isSideInvisible(BlockState bs1, BlockState bs2, Direction side) {
+        return super.isSideInvisible(bs1, bs2, side) || bs2.getBlock() == this ;
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public void animateTick(BlockState stateIn, World worldIn, BlockPos pos, Random rand) {
+        super.animateTick(stateIn, worldIn, pos, rand);
+        if (rand.nextInt(10) == 0) {
+            worldIn.addParticle(ParticleTypes.END_ROD, (double)pos.getX() + rand.nextDouble(), (double)pos.getY() + rand.nextDouble(), (double)pos.getZ() + rand.nextDouble(), 0.0D, 0.0D, 0.0D);
+        }
+
+    }
+
+    public boolean isTransparent(BlockState state) {
+        return true;
     }
 }
