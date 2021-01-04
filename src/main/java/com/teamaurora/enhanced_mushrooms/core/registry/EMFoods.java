aef1fff95e7ebcea799f31461670f91917a934d3
Epic312 <kaden_ostendorf@yahoo.com>
2021-01-03 18:28:06 -0700

    slight nerf to beef stew saturation

--- src/main/java/com/teamaurora/enhanced_mushrooms/core/registry/EMFoods.java
+++ src/main/java/com/teamaurora/enhanced_mushrooms/core/registry/EMFoods.java
@@ -4,5 +4,5 @@ import net.minecraft.item.Food;
 
 public class EMFoods {
     public static final Food ROASTED_MUSHROOMS = new Food.Builder().hunger(2).saturation((0.2F)).build();
-    public static final Food BEEF_STEW = new Food.Builder().hunger(10).saturation(1.0F).build();
+    public static final Food BEEF_STEW = new Food.Builder().hunger(10).saturation(0.8F).build();
 }
