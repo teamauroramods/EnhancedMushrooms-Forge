98f0ba7aa3c9ca97246f0a59430ee18563a75675
Epic312 <kaden_ostendorf@yahoo.com>
2021-06-14 11:17:29 -0600

    fixed #59

--- src/main/java/com/teamaurora/enhanced_mushrooms/core/registry/util/EMBlockSubRegistryHelper.java
+++ src/main/java/com/teamaurora/enhanced_mushrooms/core/registry/util/EMBlockSubRegistryHelper.java
@@ -2,16 +2,18 @@ package com.teamaurora.enhanced_mushrooms.core.registry.util;
 
 import com.minecraftabnormals.abnormals_core.common.blocks.sign.AbnormalsStandingSignBlock;
 import com.minecraftabnormals.abnormals_core.common.blocks.sign.AbnormalsWallSignBlock;
-import com.minecraftabnormals.abnormals_core.common.items.AbnormalsSignItem;
+import com.minecraftabnormals.abnormals_core.core.api.SignManager;
 import com.minecraftabnormals.abnormals_core.core.util.registry.BlockSubRegistryHelper;
 import com.minecraftabnormals.abnormals_core.core.util.registry.RegistryHelper;
 import com.mojang.datafixers.util.Pair;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
+import net.minecraft.block.WoodType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.SignItem;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.RegistryObject;
@@ -22,12 +24,12 @@ public class EMBlockSubRegistryHelper extends BlockSubRegistryHelper {
     }
 
     public Pair<RegistryObject<AbnormalsStandingSignBlock>, RegistryObject<AbnormalsWallSignBlock>> createCompatSignBlock(String name, MaterialColor color, String modid) {
-        ResourceLocation texture = new ResourceLocation(this.parent.getModId(), "textures/entity/signs/" + name + ".png");
-        RegistryObject<AbnormalsStandingSignBlock> standing = this.deferredRegister.register(name + "_sign", () -> new AbnormalsStandingSignBlock(net.minecraft.block.AbstractBlock.Properties.create(Material.WOOD).doesNotBlockMovement().hardnessAndResistance(1.0F).sound(SoundType.WOOD), texture));
-        RegistryObject<AbnormalsWallSignBlock> wall = this.deferredRegister.register(name + "_wall_sign", () -> new AbnormalsWallSignBlock(net.minecraft.block.AbstractBlock.Properties.create(Material.WOOD, color).doesNotBlockMovement().hardnessAndResistance(1.0F).sound(SoundType.WOOD).lootFrom(standing.get()), texture));
+        WoodType type = SignManager.registerWoodType(WoodType.create(this.parent.getModId() + ":" + name));
+        RegistryObject<AbnormalsStandingSignBlock> standing = this.deferredRegister.register(name + "_sign", () -> new AbnormalsStandingSignBlock(Block.Properties.create(Material.WOOD).doesNotBlockMovement().hardnessAndResistance(1.0F).sound(SoundType.WOOD), type));
+        RegistryObject<AbnormalsWallSignBlock> wall = this.deferredRegister.register(name + "_wall_sign", () -> new AbnormalsWallSignBlock(Block.Properties.create(Material.WOOD, color).doesNotBlockMovement().hardnessAndResistance(1.0F).sound(SoundType.WOOD).lootFrom(standing.get()), type));
         ItemGroup determinedGroup = !ModList.get().isLoaded(modid) && modid.equals("indev") ? null : ItemGroup.DECORATIONS;
 
-        this.itemRegister.register(name + "_sign", () -> new AbnormalsSignItem(standing.get(), wall.get(), (new Item.Properties()).maxStackSize(16).group(determinedGroup)));
+        this.itemRegister.register(name + "_sign", () -> new SignItem(new Item.Properties().maxStackSize(16).group(determinedGroup), standing.get(), wall.get()));
         return Pair.of(standing, wall);
     }
 }
